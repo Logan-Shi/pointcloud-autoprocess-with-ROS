105af4c95837e2bcebeb699339a588f124c53b88
Logan-Shi <837898231@qq.com>
2021-03-12 13:03:04 +0800

    added bools 4 publisher

--- pcl_pub/src/gocator_publisher.cpp
+++ pcl_pub/src/gocator_publisher.cpp
@@ -19,58 +19,72 @@ int main (int argc, char **argv)
     //Read PLYFile
     std::string path = ros::package::getPath("gocator_publisher");
     std::string filename = "monkey.ply";
+    bool is_cropped = false;
+    bool is_transformed = false;
+    int pub_rate = 1;
+
     nh.getParam("filename",filename);
+    nh.getParam("is_cropped",is_cropped);
+    nh.getParam("is_transformed",is_transformed);
+    nh.getParam("pub_rate",pub_rate);
+
+    ros::Rate loop_rate(pub_rate);
     if (pcl::io::loadPLYFile(path + "/model/test/" + filename, *cloud) < 0)
     {
       PCL_ERROR ("Error loading cloud.\n");
       return (-1);
     }
 
-    //Crop box filtering
-    pcl::CropBox<PointT> box_filter(true); //crop out other points
-    Eigen::Vector4f box_center(0,
-                               -1,
-                               0,
-                               1.0);
-    double box_size = 2;
-    Eigen::Vector4f box_min(box_center(0) - box_size/2,
-                            box_center(1) - box_size/2,
-                            box_center(2) - box_size/2,
-                            1.0);
-    Eigen::Vector4f box_max(box_center(0) + box_size/2,
-                            box_center(1) + box_size/2,
-                            box_center(2) + box_size/2,
-                            1.0);
-    box_filter.setMin(box_min);
-    box_filter.setMax(box_max);
-    box_filter.setInputCloud(cloud);
-    box_filter.filter(*cloud);
+    if (is_cropped)
+    {
+        //Crop box filtering
+        pcl::CropBox<PointT> box_filter(true); //crop out other points
+        Eigen::Vector4f box_center(0,
+                                   -1,
+                                   0,
+                                   1.0);
+        double box_size = 2;
+        Eigen::Vector4f box_min(box_center(0) - box_size/2,
+                                box_center(1) - box_size/2,
+                                box_center(2) - box_size/2,
+                                1.0);
+        Eigen::Vector4f box_max(box_center(0) + box_size/2,
+                                box_center(1) + box_size/2,
+                                box_center(2) + box_size/2,
+                                1.0);
+        box_filter.setMin(box_min);
+        box_filter.setMax(box_max);
+        box_filter.setInputCloud(cloud);
+        box_filter.filter(*cloud);
+    }
 
-    // Defining a rotation matrix and translation vector
-    Eigen::Matrix4d transformation_matrix = Eigen::Matrix4d::Identity ();
-  
-    // A rotation matrix (see https://en.wikipedia.org/wiki/Rotation_matrix)
-    double theta = M_PI / 8;  // The angle of rotation in radians
-    transformation_matrix (0, 0) = std::cos (theta);
-    transformation_matrix (0, 1) = -sin (theta);
-    transformation_matrix (1, 0) = sin (theta);
-    transformation_matrix (1, 1) = std::cos (theta);
-  
-    // A translation on Z axis (0.4 meters)
-    transformation_matrix (2, 3) = 0.4;
-  
-    // Display in terminal the transformation matrix
-    std::cout << "Applying this rigid transformation to: cloud_in -> cloud_icp" << std::endl;
-    print4x4Matrix (transformation_matrix);
-  
-    // Executing the transformation
-    pcl::transformPointCloud (*cloud, *cloud, transformation_matrix);
+    if (is_transformed)
+    {
+        // Defining a rotation matrix and translation vector
+        Eigen::Matrix4d transformation_matrix = Eigen::Matrix4d::Identity ();
+      
+        // A rotation matrix (see https://en.wikipedia.org/wiki/Rotation_matrix)
+        double theta = M_PI / 8;  // The angle of rotation in radians
+        transformation_matrix (0, 0) = std::cos (theta);
+        transformation_matrix (0, 1) = -sin (theta);
+        transformation_matrix (1, 0) = sin (theta);
+        transformation_matrix (1, 1) = std::cos (theta);
+      
+        // A translation on Z axis (0.4 meters)
+        transformation_matrix (2, 3) = 0.4;
+      
+        // Display in terminal the transformation matrix
+        std::cout << "Applying this rigid transformation to: cloud_in -> cloud_icp" << std::endl;
+        print4x4Matrix (transformation_matrix);
+      
+        // Executing the transformation
+        pcl::transformPointCloud (*cloud, *cloud, transformation_matrix);
+    }
 
     //Convert the cloud to ROS message
     pcl::toROSMsg(*cloud, output);
-    output.header.frame_id = "gocator_pcl";
+    output.header.frame_id = "gocator_point_cloud";
  
-    ros::Rate loop_rate(1);
     while (ros::ok())
     {
         pcl_pub.publish(output);
